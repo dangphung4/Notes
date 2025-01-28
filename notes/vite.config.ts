0c10431ab991e160eef793cd8dad126f57a68b6d
Dang <dangphung4@gmail.com>
2025-01-27 19:17:58 -0500

    chore: fix build and manifest logging

--- notes/vite.config.ts
+++ notes/vite.config.ts
@@ -2,6 +2,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,37 +22,37 @@ export default defineConfig({
       theme_color: '#000000',
       icons: [
         {
-          src: '/src/assets/note.svg',
+          src: '/assets/note.svg',
           sizes: '200x200',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note512.svg',
+          src: '/assets/note512.svg',
           sizes: '512x512',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note192.svg',
+          src: '/assets/note192.svg',
           sizes: '192x192',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note180.svg',
+          src: '/assets/note180.svg',
           sizes: '180x180',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note167.svg',
+          src: '/assets/note167.svg',
           sizes: '167x167',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note144.svg',
+          src: '/assets/note144.svg',
           sizes: '144x144',
           type: 'image/svg+xml',
         },
         {
-          src: '/src/assets/note32.svg',
+          src: '/assets/note32.svg',
           sizes: '32x32',
           type: 'image/svg+xml',
         },
@@ -77,4 +78,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'public',
+  assetsInclude: ['**/*.svg'],
 })
